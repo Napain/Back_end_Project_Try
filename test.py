e7b08df987d021e0034daab804d354d3e892e9cc
Napain <storrealba24@gmail.com>
2024-06-28 20:23:13 +0200

    Before commit

--- test.py
+++ test.py
@@ -29,7 +29,9 @@ class ItemTests(unittest.TestCase):
 
             hashed_password = generate_password_hash('testpassword').decode('utf-8')
 
+
             user = User(username='testuser', email='test@example.com', password=hashed_password)
+
 
             db.session.add(user)
 
@@ -83,7 +85,6 @@ class ItemTests(unittest.TestCase):
 
         return response
 
- 
 
     def test_add_post(self):
 
@@ -99,8 +100,70 @@ class ItemTests(unittest.TestCase):
 
         self.assertIn(b'Your post as been created!', response.data)
 
- 
 
+    def test_add_post_wrong_input(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        response = self.add_post('Test Post', 'as', "s")
+
+        self.assertEqual(response.status_code, 200)
+
+        post = Post.query.filter_by(title='Test Post').first()
+
+
+
+        self.assertIn(b'This field is required.', response.data)
+
+    def test_add_post_no_content(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        response = self.add_post('Test Post', '', 5)
+
+        self.assertEqual(response.status_code, 200)
+
+
+        self.assertIn(b'This field is required.', response.data)
+
+    def test_add_post_no_title(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        response = self.add_post('', 'content', 5)
+
+        self.assertEqual(response.status_code, 200)
+
+
+        self.assertIn(b'This field is required.', response.data)
+
+    def test_add_post_no_price(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        response = self.add_post('', 'content', "")
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn(b'This field is required.', response.data)
+
+
+    def test_add_post_special(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        response = self.add_post('Test Pos @t', 'This is a test post.', 10)
+
+        self.assertEqual(response.status_code, 200)
+
+        post = Post.query.filter_by(title='Test Pos @t').first()
+
+        self.assertIsNotNone(post)
+
+        self.assertIn(b'Your post as been created!', response.data)
+
+
+ 
     def test_delete_post(self):
 
         self.login('test@example.com', 'testpassword')
@@ -118,8 +181,31 @@ class ItemTests(unittest.TestCase):
         post = Post.query.get(post.id)
 
         self.assertIsNone(post)
+        
 
- 
+    def test_delete_post_special(self):
+
+        self.login('test@example.com', 'testpassword')
+
+        self.add_post('Test @@', 'This is a test post.', 10)
+
+        post = Post.query.filter_by(title='Test @@').first()
+
+        self.assertIsNotNone(post)
+
+        response = self.delete_post(post.id)
+
+        self.assertEqual(response.status_code, 200)
+
+        post = Post.query.get(post.id)
+
+        self.assertIsNone(post)
+
+
+
+
+    #  Test deleting non-existent item
+
 
 if __name__ == "__main__":
 
